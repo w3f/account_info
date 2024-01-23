24ebc7f0e8cb04ddceb065f0e40ff25d2dc8482c
Piet Wolff <piet.wolff@gmail.com>
2024-01-23 17:49:56 +0100

    init

--- info.py
+++ info.py
@@ -4,57 +4,144 @@ import time
 import json
 import requests
 
+from dotenv import load_dotenv
+from datetime import datetime
+
+import os
+
+# load the .env file
+load_dotenv()
+
+# get the key from the .env file
+api_key = os.getenv('API_KEY')
+
 # Constants
 
 # Headers needed for receiving content
 HEADERS = {
     'Content-Type': 'application/json',
-    'X-API-Key': API_KEY,
+    'X-API-Key': api_key,
 }
 
 
 # Arguments:
 # 1: Network - Accepts "kusama" or "polkadot"
 # 1: Address - Address of account to research
+
+# the following was changed in favor of a .env file.
 # 3: API Key - Your subscan API key. See https://support.subscan.io/#introductionb
 
 
-# Check if account is verified - returns string "Yes" or "No"
-def account_verified(address):
-    endpoint = "https://" + NETWORK.lower() + ".api.subscan.io/api/v2/scan/accounts"
+# Once gather the events related to the account
+def fetch_events(address):
+    # The endpoint for events
+    endpoint = "/api/v2/scan/events"
+
+    # Full URL
+    url = f"{BASE_URL}{endpoint}"
+
+    # Parameters for the request, adjust if necessary
+    params = json.dumps({
+        "address": address,
+        "page": 0,
+        "row": 100
+    })
+
+    # Send the GET request to the API
+    response = requests.request("POST", url, headers=HEADERS, data=params)
+    
+    # Check if the request was successful
+    if response.status_code != 200:
+        print("fetching events failed with status code: " + response.status_code)
+        sys.exit(1)
+    else:
+        # Convert the response to JSON
+        data = response.json()
+        return data
+
+# Once gather the events related to the account
+def fetch_account_data(address):
+    endpoint = "/api/v2/scan/accounts"
     json_data = {
+        'address': [address],
         'row': 10,
         'page': 0,
-        'address': [address],
     }
-    r = requests.post(endpoint, headers=HEADERS, json=json_data)
-    rj = json.loads(r.text)
+    url = f"{BASE_URL}{endpoint}"
+    r = requests.post(url, headers=HEADERS, json=json_data)
+    result = json.loads(r.text)
+    return result
 
-    try:
-        verified = rj['data']['list'][0]['account_display']['identity']
-        return verified
-    except:
-        return "No"
+# Check if account is verified - returns string "Yes" or "No"
+def account_verified(address):
+    
+    verified = ACCOUNT_DATA['data']['list'][0]['registrar_info']
+
+    if verified == None:
+        verified = "NOT VERIFIED"
+    else:
+        verified = "VERIFIED"
+
+    return verified
+
 
 # Get account identity information.
 # This has to be done via the  api/v2/scan/search endpoint
 # Return string with any identity information
-def get_account_identity_info(address):
-    return "meow"
+def print_account_identity_info():
+    identity_info = ACCOUNT_DATA['data']['list'][0]['account_display']
+    # check for most identity items
+
+    # List of items you want to check in the dictionary
+    items_to_check = ["display", "legal", "web", "riot", "email", "twitter"]
+    
+    # Check if the identity_info is a dictionary and has content
+    if isinstance(identity_info, dict) and identity_info:
+        for item in items_to_check:
+            # Check if the item is in the dictionary and has a non-empty value
+            if item in identity_info and identity_info[item]:
+                print(f"{item}: {identity_info[item]}  {verified}")
+    else:
+        print("No identity information available.")
+    
 
 # Get earliest identity set date (i.e. earliest date a
 # an identity.IdentitySet event occurred). Note that you need to check
 # for events, not extrinsics, because the extrinsic won't show up
 # if a proxy was used.
-def get_identity_set_date(address):
-    return "1 Jan 1970"
+def get_identity_set_date(address):  
+    # search for first event that sets the on-chain identity
+    first_identity_event = None
+    for event in EVENTS['data']['events']:
+        # Check if the event concerns setting the identity
+        if event['event_id'] == 'IdentitySet':
+            first_identity_event = event
+
+    if first_identity_event == None:
+        return "no record found"
+    if  'block_timestamp' in first_identity_event:
+        block_timestamp = int(first_identity_event['block_timestamp'])    
+        timestamp = datetime.utcfromtimestamp(block_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        return timestamp 
 
 # Get earliest identity verified date (i.e. earliest date a
 # an identity.JudgementGiven event occurred). Note that you need to check
 # for events, not extrinsics, because the extrinsic won't show up
 # if a proxy was used.
 def get_identity_verified_date(address):
-    return "1 Jan 1970"
+    # search for first event that sets the on-chain identity
+    first_identity_verified_event = None
+    for event in EVENTS['data']['events']:
+        # Check if the event concerns setting the identity
+        if event['event_id'] == 'JudgementGiven':
+            first_identity_verified_event = event
+    
+    if first_identity_verified_event == None:
+        return "no record found"
+    if 'block_timestamp' in first_identity_verified_event:
+        block_timestamp = int(first_identity_verified_event['block_timestamp'])    
+        timestamp = datetime.utcfromtimestamp(block_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        return timestamp 
 
 # Get any other treasury proposals made by this account
 # OpenGov TPs: Generally you can check for referenda.DecisionDepositPlaced
@@ -77,7 +164,8 @@ def print_out_final(address,
                     other_treasury_proposals):
     print("Address: " + str(address))
     print("Verified?: " + str(verified))
-    print("Verified Information: " + str(verified_info))
+    print("Identity Information: ")
+    print_account_identity_info()
     print("Identity first set: " + str(identity_set_date))
     print("Identity first verified: " + str(identity_verified_date))
     print("Other treasury proposals: " + str(other_treasury_proposals))
@@ -88,38 +176,34 @@ def print_out_final(address,
 # Read in args from command line
 
 ARGS_LEN = len(sys.argv) - 1
-if ARGS_LEN != 3:
+if ARGS_LEN != 2:
     print("Usage: python info.py NETWORK ACCOUNT API_KEY")
     print("NETWORK - either 'kusama' or 'polkadot' (without quotes)")
     print("ADDRESS - account to research")
-    print("API Key - Your subscan API key. See https://support.subscan.io/")
     sys.exit(1)
 else:
     
     NETWORK = sys.argv[1].lower()
+    BASE_URL = "https://" + NETWORK + ".api.subscan.io"
     ADDRESS = sys.argv[2]
-    API_KEY = sys.argv[3]
 
 
 
 # Check if you should connect to Polkadot or Kusama API endpoints
 if NETWORK == 'kusama' or NETWORK == 'polkadot':
-    print("Connecting to ' + str(NETWORK) + ' Subscan API endpoint.")
+    print("Connecting to " + str(NETWORK) + " Subscan API endpoint.")
 else:
     print("Accepted networks are KUSAMA or POLKADOT (case-insensitive)")
     sys.exit(1)
-
-print("Connecting to " + str(NETWORK) + " Subscan API endpoint.")
-
-
-
+ACCOUNT_DATA = fetch_account_data(ADDRESS)
+EVENTS = fetch_events(ADDRESS)
 verified = account_verified(ADDRESS)
-identity_info = get_account_identity_info(ADDRESS)
+#identity_info = get_account_identity_info(ACCOUNT_DATA)
 identity_set_date = get_identity_set_date(ADDRESS)
 identity_verified_date = get_identity_verified_date(ADDRESS)
 other_tps = get_other_tps(ADDRESS)
 
-print_out_final(ADDRESS, verified, identity_info, identity_set_date,
+print_out_final(ADDRESS, verified, print_account_identity_info, identity_set_date,
                 identity_verified_date, other_tps)
 
 sys.exit(0)
